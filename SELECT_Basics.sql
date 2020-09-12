81cbc203a19ec3e5158ee04330f2316d5e024c63
jacobo12montes <57403326+jacobo12montes@users.noreply.github.com>
2020-09-12 18:18:54 -0500

    Create the  querys for the world table

--- SELECT_Basics.sql
+++ SELECT_Basics.sql
@@ -1,32 +1,23 @@
 -- Show the population of Germany
 SELECT
     population
-FROM 
-    world
+FROM world
 WHERE 
     name = 'Germany';
 -- Show the name and the population for 'Sweden', 'Norway' and 'Denmark'
 SELECT
     name,
     population
-FROM 
-    world
+FROM world
 WHERE
     name IN ('Sweden', 'Norway', 'Denmark');
 -- Show the country and the area for countries with an area between 200,000 and 250,000
 SELECT 
     name, 
     area
-FROM
-    world
+FROM world
 WHERE 
     area BETWEEN 200000 AND 250000;
 -- QUIZ
--- 1
-SELECT 
-    name,
-    continent,
-    population
-FROM 
-    world;
--- 2
+
+ 
