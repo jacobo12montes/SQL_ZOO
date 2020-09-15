d921f168db8aae8fd64c4f18375a4d59368f49ff
jacobo12montes <57403326+jacobo12montes@users.noreply.github.com>
2020-09-14 19:55:40 -0500

    Create the  querys for the world table and quiz

--- SELECT_World.sql
+++ SELECT_World.sql
@@ -66,3 +66,19 @@ SELECT
  FROM world
  WHERE LENGTH(name) = LENGTH(capital);
  -- 12
+ SELECT 
+    name,
+    capital
+ FROM world
+ WHERE LEFT(name,1) = LEFT(capital,1) AND name <> capital;
+ --13
+SELECT
+    name
+FROM world
+WHERE name LIKE '%a%' AND 
+name  LIKE '%e%' AND 
+name  LIKE '%i%' AND 
+name  LIKE '%o%' AND 
+name  LIKE '%u%' AND 
+name NOT LIKE '% %';
+-- QUIZ
